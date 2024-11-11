b3f1a424be4e92f154e429a18c4750819b3662de
falcon do <falcomdolatabadii@gmail.com>
2024-11-11 14:43:49 +0200

    -----

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
 a=10
 print(a)
 from sklearn.linear_model import LogisticRegression
-
-model=LogisticRegression()
-model.fit(X_train,y_train)
-
-predictions= model.predict(X_test)
-predictions[:10]
+print('bye')
